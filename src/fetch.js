f4c2316c901c9fce2f03ee261780da0ca88daffe
sunkaflek <>
2020-04-25 18:35:23 +0200

    update user agent

--- src/fetch.js
+++ src/fetch.js
@@ -4,6 +4,6 @@ const fetch = require('node-fetch')
 
 module.exports = (url, args = {}) => {
   args.headers = args.headers || {}
-  args.headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:74.0) Gecko/20100101 Firefox/74.0'
+  args.headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0'
   return fetch(url, args)
 }
